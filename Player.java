dd9517d301ecfcbdec86694b749c8ce745ab7f2b
Thomas Scouler <67656155+Daksos@users.noreply.github.com>
2022-11-17 09:45:12 -0800

    Add files via upload
    Updates Board and Player Classes

---
+++ Player.java
@@ -0,0 +1,62 @@
+package martianchess;
+
+import java.awt.*;
+
+public class Player {
+    private static int numPlayers = 2;
+    private static Player players[] = new Player[numPlayers];
+    private static Player currentPlayer;
+    private Board board = new Board();
+    private Color color;
+    private int score;
+    
+    Player(){
+    }
+    
+    public static void Reset() {
+        if (players[0] == null) {
+            players[0] = new Player(Color.red);
+            players[1] = new Player(Color.yellow);
+        }
+        currentPlayer = players[0];
+    }
+    public static Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public static void switchCurrentPlayer() {
+        if (currentPlayer == players[0])
+            currentPlayer = players[1];
+        else
+            currentPlayer = players[0];
+    }    
+    public static Player getPlayer1() {
+        return players[0];
+    }
+    public static Player getPlayer2() {
+        return players[1];
+    }    
+    public static Player getPlayer3() {
+        return players[2];
+    }
+    public static Player getPlayer4() {
+        return players[3];
+    }    
+    public Board getBoard(){
+        return(board);
+    }
+    public Player(Color _color) {
+        color = _color;
+        score = 0;
+    }    
+    public Color getColor() {
+        return (color);
+    }
+    public void addScore(int _value)
+    {
+        score += _value;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+}
